e4349594a6f244718fd080b7b6f70198c8f2480c
Tony Ivanov <telamohn@gmail.com>
2013-08-04 22:42:50 +0200

    Got a binary compiling

--- jni/Android.mk
+++ jni/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := hello-jni
-LOCAL_SRC_FILES := hello-jni.c
+LOCAL_MODULE    := fbstream
+LOCAL_SRC_FILES := fbstream.cc
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
---
+++ jni/fbstream.cc
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "fbstream.h"
+
+int main(int argc, char **argv){
+  printf( "Hello Bork!\n");
+  return 0;
+}
